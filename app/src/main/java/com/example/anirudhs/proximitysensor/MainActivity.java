29e62281c85123c6a253429860db9b637ce6de4f
Anirudh S <anirudh@gmail.com>
2017-06-29 18:34:08 +0530

    Final

--- app/src/main/java/com/example/anirudhs/proximitysensor/MainActivity.java
+++ app/src/main/java/com/example/anirudhs/proximitysensor/MainActivity.java
@@ -7,24 +7,27 @@ import android.hardware.SensorManager;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.CountDownTimer;
 import android.os.Handler;
 import android.os.Message;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
-    TextView myText,timerText;
+    TextView myText, timerText;
     SensorManager sm;
     Sensor proxSensor;
     Uri notification;
-    Ringtone r=null;
+    Ringtone r = null;
     String text;
-
-
+    long time;
+    boolean work=false;
+    CountDownTimer myTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +35,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         setContentView(R.layout.activity_main);
 
         myText = (TextView) findViewById(R.id.myText);
-
+        timerText = (TextView)findViewById(R.id.timerText);
         sm = (SensorManager) getSystemService(SENSOR_SERVICE);
         proxSensor = sm.getDefaultSensor(Sensor.TYPE_PROXIMITY);
 
@@ -64,25 +67,72 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     }
 
 
-
     @Override
     public void onSensorChanged(SensorEvent event) {
         final float distance = event.values[0];
 
-        if(distance==3.0){
+        if (distance <= 3.0) {
 
-
-            text="User Near";
-
+            text = "User Near";
+            myText.setText(text);
+            new Operation().execute();
 
         }
-        else{
+        else {
+
+            r.stop();
             text = "User Far";
+            myText.setText(text);
+            work=true;
+            timerText.setVisibility(View.GONE);
+        }
+    }
+
+
+    private class Operation extends AsyncTask<Void,Void,Void> {
+
+        @Override
+        protected void onPreExecute() {
+            time =10;
+            timerText.setVisibility(View.VISIBLE);
+            timerText.setText(String.valueOf(time));
+            work=false;
 
         }
-        myText.setText(text);
+
+        @Override
+        protected void onCancelled() {
+            work=true;
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+         timerText.setText(String.valueOf(time));
+            if(time==0){
+                r.play();
+                timerText.setVisibility(View.GONE);
+                cancel(true);
+            }
+            time--;
+        }
 
 
 
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            while (time >=0 && !work) {
+                publishProgress();
+
+                try {
+                    Thread.sleep(1000);
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+                return null;
+
+        }
     }
 }
